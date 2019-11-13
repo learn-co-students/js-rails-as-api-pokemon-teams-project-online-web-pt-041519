b555fcebe0da1119f3c670e2ffbad446fa27efcc
aparkening <aaron.parkening@gmail.com>
2019-11-13 17:42:56 -0500

    Remove extraneous comments.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -2,20 +2,10 @@ class PokemonsController < ApplicationController
 
   # Add Pokemon to trainer if trainer team < 6
   def create    
-
-    # byebug
-
-
-    # # Set params
-    # params[:pokemon][:nickname] = Faker::Name.first_name if !params[:nickname]
-
-    # params[:pokemon][:species] = Faker::Games::Pokemon.name if !params[:species]
-
     name = Faker::Name.first_name
     species = Faker::Games::Pokemon.name
-    pokemon = Pokemon.new(nickname: name, species: species, trainer_id: params[:trainer_id])
 
-    # pokemon = Pokemon.new(params)
+    pokemon = Pokemon.new(nickname: name, species: species, trainer_id: params[:trainer_id])
 
     if pokemon.save
       # Send return json
@@ -33,12 +23,4 @@ class PokemonsController < ApplicationController
     pokemon.destroy
   end
 
-  # private
- 
-  # def poke_params
-  #   params.require(:pokemon).permit(:nickname, :species, :trainer_id)
-  # end
-
 end
-
-
--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -2,7 +2,6 @@ class TrainersController < ApplicationController
 
   def index
     trainers = Trainer.all
-    # render json: trainers, include: [:pokemons]
 
     render json: TrainerSerializer.new(trainers).to_serialized_json
   end
