175d3f938276ba0bec67d6769582bd6f46bfc2ce
Michael Chen <michaelchengaming@gmail.com>
2019-11-18 21:25:10 -0800

    refactor and tidy up, everything works.. WUBBA LUBBA DUB DUB

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,29 +5,19 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`;
 // set variable for the main container
 const mainContainer = document.querySelector('main');
 
-// console.log(mainContainer)
-
 // after the page loads, we do this
 document.addEventListener("DOMContentLoaded", (e) => {
-    // let trainers = fetchTrainers();
-    // console.log(trainers)
-    // trainers.forEach(trainer => {
-    //     renderTrainers(trainer);
-    // });
    fetchTrainers().then(trainers => {
         trainers.forEach(trainer => {
-            // console.log(trainer);
             renderTrainers(trainer);
         });
     })
-    .catch(error => mainContainer.innerHTML = error.message);
 });
 
 // fetches and returns the json of all the trainers with their pokemans
 async function fetchTrainers(){
     const res = await fetch(TRAINERS_URL);
     const trainerJson = await res.json();
-    // console.log(trainerJson);
     return trainerJson;
 };
 
@@ -38,64 +28,41 @@ function renderTrainers(trainer){
     pokeContainer.setAttribute("data-id", `${trainer.id}`); 
     pokeContainer.innerHTML = `<p>${trainer.name}</p><button data-trainer-id="${trainer.id}">Add Pokemon</button><ul></ul>`;
     let pokeList = pokeContainer.querySelector("ul");
-    // renderPokemons(trainer, pokeList);
+    // Iterate over each pokemon and render them on the card
     trainer.pokemons.forEach(pokemon => {
         const pokeLi = renderPokemon(pokemon);
         pokeList.appendChild(pokeLi);
     });
-    // trainer.pokemons.forEach(pokemon => {
-    //     console.log(pokemon);
-    //     const pokeLi = document.createElement("li");
-    //     pokeLi.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
-    //     pokeList.appendChild(pokeLi);
-    // });
-
-
+    // Give button joining feature
     const addBtn = pokeContainer.querySelector("button");
     addBtn.addEventListener("click", (e) => {
         addPokemon(e).then(p => {
             const pokeLi = renderPokemon(p);
+            let pokeName = pokeLi.innerText.split(' ')[0];
+            alert(`${pokeName} has joined your team`);
             pokeList.appendChild(pokeLi);
         });
-        // console.log(p)
     });
-
-    // trainer.pokemons.forEach(pokemon => {
-    //     const pokeLi = document.createElement("li");
-    //     pokeLi.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
-
-    //     const deleteBtn = pokeLi.querySelector("button");
-    //     // console.log(deleteBtn);
-    //     deleteBtn.addEventListener("click", (e) => {
-    //         bootFromTeam(e);
-    //     });
-        
-    //     pokeList.appendChild(pokeLi)
-    
+    // Append to main to build each trainer card
     mainContainer.appendChild(pokeContainer);
-    // });
 };
 
-// add pokemons to trainer container
+// Add pokemons to trainer container
 function renderPokemon(pokemon){
-    // trainer.pokemons.forEach(pokemon => {
-        const pokeLi = document.createElement("li");
-        pokeLi.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`;
-        const deleteBtn = pokeLi.querySelector("button");
-        // console.log(deleteBtn);
-        deleteBtn.addEventListener("click", (e) => {
-            bootFromTeam(e);
-        });
-        // list.appendChild(pokeLi);
-        return pokeLi
-    // });
+    const pokeLi = document.createElement("li");
+    pokeLi.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`;
+    // Give button delete feature
+    const deleteBtn = pokeLi.querySelector("button");
+    deleteBtn.addEventListener("click", (e) => {
+        bootFromTeam(e);
+    });
+    return pokeLi;
 };
 
 // Remove pokemon from team
 async function bootFromTeam(event){
-    // console.log(event.target.getAttribute("data-pokemon-id"))
     const pokeId = event.target.getAttribute("data-pokemon-id");
-    // console.log(POKEMONS_URL + '/' + pokeId)
+    const pokeName = event.target.parentNode.innerText.split(' ')[0];
     try {
         fetch(POKEMONS_URL + '/' + pokeId, {
         method: "DELETE",
@@ -105,17 +72,17 @@ async function bootFromTeam(event){
         },
         body: JSON.stringify(pokeId)
         });
-        alert("This pokemon is sent to daycare.");
+        alert(`${pokeName} is sent to the daycare.`);
         event.target.parentNode.remove();
     } catch(error) {
-        event.target.parentNode.parentNode.innerHTML = "<h1>something went wrong</h1>"
+        event.target.parentNode.parentNode.innerHTML = "<h1>Something went wrong. The Pokemon is too clingy to leave.</h1>";
     };
 
 };
 
 // post request to add a new pokemon
 async function addPokemon(event){
-    const trainerId = event.target.getAttribute("data-trainer-id")
+    const trainerId = event.target.getAttribute("data-trainer-id");
     let configObj = {
         method: "POST",
         headers: {
@@ -124,28 +91,11 @@ async function addPokemon(event){
         },
         body: JSON.stringify({trainer_id: trainerId})
     };
-    let response = await fetch(POKEMONS_URL, configObj);
-    let pokeJson = await response.json();
-    return pokeJson;
+    try {
+        let response = await fetch(POKEMONS_URL, configObj);
+        let pokeJson = await response.json();
+        return pokeJson;
+    } catch(error) {
+        event.parentNode.innerHTML =`<h2>Run the server you ding dong.</h2> ${error.message}`;
+    };
 };
-// let pokeFormData = {
-//     id: 
-//     name: 
-//     species: 
-//     trainer_id:
-// };
-
-function newPokemon(){
-    let configObj = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(pokeFormData)
-};
-
-fetch(POKEMONS_URL, configObj)
-.then(res => res.json)
-.then(json)
-}
