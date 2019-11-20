888d2a8935ab8f0455d7693b9afe85889c907390
Erin Ponsonby <erinponsonby@gmail.com>
2019-11-20 09:52:50 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -31,31 +31,47 @@ function createCards(object) {
         newDiv.appendChild(p)
         newDiv.appendChild(addButton)
 
-        addButton.addEventListener("click", createNewPokemon)
-
         let ul = document.createElement("ul")
+        ul.setAttribute('ul-trainer-id', object[i].id)
             for (let n = 0; n < object[i].pokemons.length; n++) {
                 let li = document.createElement("li")
                 li.textContent = `${object[i].pokemons[n].nickname} (${object[i].pokemons[n].species})`
+                li.setAttribute('li-pokemon-id', object[i].pokemons[n].id)
                 let button = document.createElement("button")
                 button.className = "release"
                 button.textContent = "Release"
                 button.setAttribute('data-pokemon-id', object[i].pokemons[n].id)
+
+                button.addEventListener("click", function(event){
+                    event.preventDefault
+                    releasePokemon(button.attributes[1].value)
+                })
+
                 li.appendChild(button)
-                newDiv.appendChild(li)
+                ul.appendChild(li)
             }
+        
+            addButton.addEventListener("click", function(){
+                if (document.querySelectorAll(`[ul-trainer-id="${object[i].id}"] li`).length < 6) {
+                    createNewPokemon(object[i].id)
+                }
+            })
+
+
         newDiv.appendChild(ul)
         let main = document.getElementsByTagName("main")[0]
         main.appendChild(newDiv)
     }
 }
 
-function createNewPokemon() {
+function createNewPokemon(id) {
     let form = document.getElementsByTagName("form")[0]
-    form.submit()
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
+    })
 
     let pokemonData = {
-        trainer_id: "1"
+        trainer_id: id
     }
 
     let configObject = {
@@ -69,5 +85,43 @@ function createNewPokemon() {
 
     fetch(POKEMONS_URL, configObject)
     .then(res => res.json())
-    .then(object => console.log(object))
+    .then(object => addNewPokemon(object))
+}
+
+function addNewPokemon(object){
+    let ul = document.querySelector(`[ul-trainer-id = '${object.trainer_id}']`)
+    let li = document.createElement("li")
+    li.textContent = `${object.nickname} (${object.species})`
+    let releaseButton = document.createElement("button")
+    releaseButton.className = "release"
+    releaseButton.textContent = "Release"
+    releaseButton.setAttribute('data-pokemon-id', object.id)
+    li.appendChild(releaseButton)
+    ul.appendChild(li)
+}
+
+
+function releasePokemon(id) {
+
+    let pokemonData = {
+        pokemon_id: id
+    }
+
+    let configObject = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(pokemonData)
+    }
+
+    fetch(`http://localhost:3000/pokemons/${id}`, configObject)
+    .then(res => res.json())
+    .then(object => deletePokemon(object))
+}
+
+function deletePokemon(object) {
+    let element = document.querySelectorAll(`[li-pokemon-id="${object.id}"]`)[0]
+    element.parentNode.removeChild(element)
 }
